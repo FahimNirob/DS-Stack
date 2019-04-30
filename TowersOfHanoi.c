efb9b058205008f783ae5e5c2868bf6f3233c392
Fahim Nirob <fahimn56@gmail.com>
2019-04-30 22:30:33 +0600

    Towers of Hanoi program added by Fahim Nirob. This problem is like a game.

---
+++ TowersOfHanoi.c
@@ -0,0 +1,26 @@
+// Towers of Hanoi problem
+
+#include<stdio.h>
+int Tower(int n,char BEG,char AUX,char END);
+
+int main(){
+    int i;
+    printf("Enter the number of disks : ");
+    scanf("%d",&i);
+    Tower(i,'A','B','C');
+    return 0;
+}
+
+int Tower(int n,char BEG,char AUX,char END){
+    if(n==1){
+        printf("%c -> %c \n",BEG,END);
+        return ;
+    }
+    Tower(n-1,BEG,END,AUX);
+    printf("%c -> %c\n",BEG,END);
+
+    Tower(n-1,AUX,BEG,END);
+    return ;
+
+}
+
